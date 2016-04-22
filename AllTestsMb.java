d13b8088e74769ac34d8d5628b289f8fe6c11d79
Ronanmorais <ronan_fm100@hotmail.com>
2016-04-22 01:08:30 -0300

    Classes de teste para o pacote mb
    Criei essas classes de teste para o pacote MB.
    Na classe (AgendamentoMBTest) eu não soube como passar a data como parâmetro para comparar e fazer o teste. 
    Então, deixei null.

---
+++ AllTestsMb.java
@@ -0,0 +1,11 @@
+package com.ada.mb;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ClienteMBTest.class , AgendamentoMBTest.class})
+public class AllTestsMb {
+
+}
