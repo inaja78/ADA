5d6609c83829e90c5bb14849ba14b3660c15f39f
Inaja <inaja.silva@gmail.com>
2016-04-25 21:34:00 -0300

    teste

--- src/com/ada/dao/ClienteDAO.java
+++ src/com/ada/dao/ClienteDAO.java
@@ -1,10 +1,10 @@
-package com.ada.dao;
+package  com.ada.dao;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.list;
 
 import com.ada.modelo.Cliente;
 
